935deaf2e6b672607205a7ed62cbf150175ace15
Michael Wojke <wojkem@gmail.com>
2020-02-29 19:06:05 -0500

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM python:3.8
+ADD HelloXS.py /
+
+CMD ["python", "./HelloXS.py"]
---
+++ HelloXS.py
@@ -0,0 +1,8 @@
+# My First Python App
+
+# Author = Mike Wojke
+# Maintainer = 'Mike Wojke'
+# Version = '0.2.0'
+
+print("Hello Expert Services!")
+print("DevOps or Bust!!")
