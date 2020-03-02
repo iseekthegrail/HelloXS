4db0698ab3f150d03da92870d34642cc4485ccac
Richard Bocchinfuso <rbocchinfuso@gmail.com>
2020-03-02 13:25:00 -0500

    test commit

--- HelloXS.py
+++ HelloXS.py
@@ -13,6 +13,7 @@ while True:
     cprint ('Hello Expert Services!' , 'red')
     cprint ('DevOps or Bust!!', 'green')
     cprint ('Testing Ghetto CI/CD \n','red', attrs=['reverse', 'blink'])
+    cprint ('Testing Ghetto CI/CD \n','red', attrs=['reverse', 'blink'])
     time.sleep (5)
     y += 1
 x += x
