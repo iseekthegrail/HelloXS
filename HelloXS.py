99aa0e062b728779ad0684c28cc43d1d9a46f23b
Michael Wojke <wojkem@gmail.com>
2020-05-20 23:05:18 -0400

    .

--- HelloXS.py
+++ HelloXS.py
@@ -6,5 +6,28 @@
 
 from termcolor import cprint
 
-cprint('Hello Expert Services!' , 'blue')
+cprint('Hello Expert Services!' , 'red')
 cprint('DevOps or Bust!!', 'white', 'on_blue')
+
+
+#cprint('Hello Expert Services!' , 'white', on_red)
+#cprint('DevOps or Bust!!', 'blue')
+
+
+
+#color = input('Color to display text in: ')
+#cprint('Hello Expert Services!', color)
+#cprint('DevOps or Bust!!', color)
+
+
+#### Let's add a condition
+#cprint ('running with a conditional statement', 'red', 'on_white')
+#run = input ('Do you really want to run this program? ')
+#if run == 'y' or run == 'Y':
+#  color = input('Color to display text in: ')
+#  cprint('Hello Expert Services!', color)
+#  cprint('DevOps or Bust!!', color)
+#elif run == 'n' or run == 'N':
+#  cprint ('You deciced not to contine.' ,'cyan')
+#else:
+#  cprint ('WTF, that is not a valid entry. Exiting!','red')
